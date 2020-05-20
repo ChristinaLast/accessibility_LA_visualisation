0630799aad44d9b605c7d7017fec163dc4735388
ChristinaLast <christina.last@outlook.com>
2020-05-20 22:46:09 +0100

    adding progress on data cleaning for visualisation

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-from shapely.geometry import Point, shape
+import geopandas as gpd
+
+import fiona
+# Enable fiona driver
+gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
+
+from shapely.geometry import MultiPolygon, shape
 
 from flask import Flask
 from flask import render_template
@@ -9,36 +15,48 @@ import json
 
 
 data_path = './input/'
-n_samples = 30000
 
-def get_age_segment(age):
-    if age <= 22:
-        return '22-'
-    elif age <= 26:
-        return '23-26'
-    elif age <= 28:
-        return '27-28'
-    elif age <= 32:
-        return '29-32'
-    elif age <= 38:
-        return '33-38'
+def get_accessibility_segment(Tot_r_20):
+    if Tot_r_20 <= 200000:
+        return '200000-'
+    elif Tot_r_20 <= 350000:
+        return '200000-350000'
+    elif Tot_r_20 <= 500000:
+        return '350000-500000'
+    elif Tot_r_20 <= 650000:
+        return '500000-650000'
+    elif Tot_r_20 <= 750000:
+        return '650000-750000'
     else:
-        return '39+'
+        return '750000+'
+def get_ht_ami_segment(ht_ami):
+    if ht_ami <= 45:
+        return '45%-'
+    elif ht_ami <= 55:
+        return '45%-55%'
+    elif ht_ami <= 65:
+        return '55%-65%'
+    elif ht_ami <= 75:
+        return '65%-75%'
+    elif ht_ami <= 85:
+        return '75%-85%'
+    else:
+        return '85%+'
 
-def get_location(longitude, latitude, provinces_json):
-    
-    point = Point(longitude, latitude)
+def get_location(geometry, city_json):
 
-    for record in provinces_json['features']:
+    geometry = MultiPolygon(geometry)
+
+    for record in city_json['features']:
         polygon = shape(record['geometry'])
-        if polygon.contains(point):
+        if polygon.contains(MultiPolygon):
             return record['properties']['name']
     return 'other'
 
+with open(data_path + 'geojson/Los_Angeles_Neighborhood_Map.geojson') as data_file:
+    city_json = json.load(data_file)
 
-with open(data_path + '/geojson/china_provinces_en.json') as data_file:    
-    provinces_json = json.load(data_file)
-
+city_json
 app = Flask(__name__)
 
 @app.route("/")
@@ -47,28 +65,28 @@ def index():
 
 @app.route("/data")
 def get_data():
-    gen_age_tr = pd.read_csv(data_path + 'gender_age_train.csv')
-    ev = pd.read_csv(data_path + 'events.csv')
-    ph_br_dev_model = pd.read_csv(data_path + 'phone_brand_device_model.csv')
+    all_data_block = pd.read_csv(data_path + 'Spatial_weights_pred.csv')
+    pd.set_option('display.max_columns', None)
+    all_data_city = all_data_block.drop(['Unnamed: 0','CB10','OBJECTID_1','GEOID10','CTCB10','BG10','X_CENTER','Y_CENTER','Shape_Leng','Shape_Area','BlockId','BlockgroupId','TractId'], axis=1)
+    all_data_city_mean = all_data_city.groupby('COMMNAME').mean().reset_index().drop(['Black_Afri','Hispanic','White_Alon'],axis=1)
+    all_data_ethnicity = all_data_city[['COMMNAME', 'Black_Afri','Hispanic','White_Alon']]
+    all_data_ethnicity_sum = all_data_ethnicity.groupby('COMMNAME').sum().reset_index()
+    all_data_city = all_data_city_mean.merge(all_data_ethnicity_sum, on='COMMNAME')
+    all_data_city = all_data_city.reset_index(drop=True)
+    all_data_city
+    all_data_city['COMMNAME'] = all_data_city['COMMNAME'].map(lambda x: x.lstrip('City of '))
+    all_data_city['COMMNAME'] = all_data_city['COMMNAME'].map(lambda x: x.lstrip('Unincorporated - '))
+    # Read file
+    city_geo = gpd.read_file('input/shape/Los Angeles Neighborhood Map.kml', driver='KML')
 
-    df = gen_age_tr.merge(ev, how='left', on='device_id')
-    df = df.merge(ph_br_dev_model, how='left', on='device_id')
-    #Get n_samples records
-    df = df[df['longitude'] != 0].sample(n=n_samples)
+    city_data_geo = pd.merge(all_data_city, city_geo, left_on='COMMNAME', right_on='Name')
+    city_data_geo = city_data_geo[['COMMNAME','Tot_r_10','Tot_r_20','Tot_r_50','ht_ami', 'population', 'co2_per_hh', 'autos_per_', 'pct_transi', 'res_densit', 'emp_gravit','emp_ndx','h_cost','Black_Afri','Hispanic','White_Alon','geometry']]
+
+    city_data_geo['Tot_r_20_seg'] = city_data_geo['Tot_r_20'].apply(lambda Tot_r_20: get_accessibility_segment(Tot_r_20))
 
 
-    top_10_brands_en = {'华为':'Huawei', '小米':'Xiaomi', '三星':'Samsung', 'vivo':'vivo', 'OPPO':'OPPO',
-                        '魅族':'Meizu', '酷派':'Coolpad', '乐视':'LeEco', '联想':'Lenovo', 'HTC':'HTC'}
-
-    df['phone_brand_en'] = df['phone_brand'].apply(lambda phone_brand: top_10_brands_en[phone_brand] 
-                                                    if (phone_brand in top_10_brands_en) else 'Other')
-
-    df['age_segment'] = df['age'].apply(lambda age: get_age_segment(age))
-
-    df['location'] = df.apply(lambda row: get_location(row['longitude'], row['latitude'], provinces_json), axis=1)
-
-    cols_to_keep = ['timestamp', 'longitude', 'latitude', 'phone_brand_en', 'gender', 'age_segment', 'location']
-    df_clean = df[cols_to_keep].dropna()
+    cols_to_keep = ['timestamp', 'longitude', 'latitude', 'phone_brand_en', 'gender', 'age_segment', 'geometry']
+    df_clean = city_data_geo[cols_to_keep].dropna()
 
     return df_clean.to_json(orient='records')
 
